596b20b33db4eac05a9e2beaac80dd40e7471ab8
mannuelst <manuelds@outlook.pt>
2024-02-28 00:00:01 +0100

    update: other configs

--- src/app.tsx
+++ src/app.tsx
@@ -6,10 +6,9 @@ function App() {
 
   return (
     <>
-      <p>
-        ....
-
-      </p>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </>
   )
 }
