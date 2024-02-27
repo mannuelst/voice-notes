e11f7c4f4520d97949a88cfcb85a09107e213c16
mannuelst <manuelds@outlook.pt>
2024-02-27 23:56:14 +0100

    feat: :arrow_up: add tailwind

---
+++ src/app.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>
+        ....
+
+      </p>
+    </>
+  )
+}
+
+export default App
