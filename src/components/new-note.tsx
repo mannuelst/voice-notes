941650a3a1ebd9f177ed451ebb63aa51d5f700da
mannuelst <manuelds@outlook.pt>
2024-02-29 20:58:15 +0100

    update: create new card modal

--- src/components/new-note.tsx
+++ src/components/new-note.tsx
@@ -4,7 +4,7 @@ import { X } from 'lucide-react'
 export function NewCard() {
     return (
         <Dialog.Root>
-            <Dialog.Trigger className='rounded-md flex flex-col gap3 bg-slate-700 p-5 text-left'>
+            <Dialog.Trigger className='rounded-md flex flex-col gap3 bg-slate-700 p-5 text-left  outline-none over:ring-2 hover:ring-slate-600 focus-visible:ring-lime-400'>
 
                 <span className='text-sm font-medium text-slate-200'>Adicionar nota</span>
                 <p className='leading-6 text-sm text-slate-400'>Grave uma nota em áudio que será convertido para texto automaticamente</p>
@@ -18,16 +18,16 @@ export function NewCard() {
                         </Dialog.Close>
                         <div className='flex flex-1 flex-col gap-3 p-5'>
                             <span className='text-sm font-medium text-slate-300'>
-
+                                Adicionar Nota
                             </span>
                             <p className='leading-6 text-sm text-slate-400'>
-
+                                Comece <button type="button" className='font-medium text-lime-400 hover:underline'>gravando um áudio</button> ou  <button type="button" className='font-medium text-lime-400 hover:underline'>adicionar um texto</button>.
                             </p>
 
                         </div>
                         <button type='button'
-                            className='w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group'>
-                            Deseja <span className='text-red-400 group-hover:underline'>apagar esta nota</span>?
+                            className='w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500'>
+                            Salvar nota
                         </button>
                     </Dialog.Content>
 
