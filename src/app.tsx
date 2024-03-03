8a7d924af202cfea2eea57f33f7311a03fd87fd6
mannuelst <manuelds@outlook.pt>
2024-03-03 22:47:09 +0100

    fix: :zap: seach card
    add the feat to seach cards

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import logo from './assets/Logo.svg'
 import { NewCard } from './components/new-note'
 import { NoteCard } from './components/note-card'
@@ -10,6 +10,7 @@ interface Note {
 }
 
 function App() {
+  const [search, setSearch] = useState('')
   const [notes, setNotes] = useState<Note[]>(() => {
     const noteOnStorage = localStorage.getItem('notes')
     if (noteOnStorage) {
@@ -26,6 +27,13 @@ function App() {
     setNotes([newNote, ...notes])
     localStorage.setItem('notes', JSON.stringify([newNote, ...notes]))
   }
+
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value
+    setSearch(query)
+  }
+
+  const filterNotes = search !== '' ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())) : notes
   return (
     <>
       <div className='mx-auto max-w-6xl my-12 space-y-6 '>
@@ -33,17 +41,18 @@ function App() {
         <form className='w-full'>
           <input type="text" name="" id="" placeholder='Busque em suas notas...'
             className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'
+            onChange={handleSearch}
           />
         </form>
         <div className='h-px bg-slate-700' />
         <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
           <NewCard onNoteCreated={onNoteCreated} />
           {
-            notes.map(note => {
+            filterNotes.map(note => {
               return <NoteCard key={note.id} note={note} />
             })
           }
-          <NoteCard note={{ date: new Date(), content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.' }} />
+          {/* <NoteCard note={{ date: new Date(), content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.' }} /> */}
         </div>
 
 
